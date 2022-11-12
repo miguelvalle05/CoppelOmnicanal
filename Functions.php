4952e7fd1077a7769ee6d4e9a6722dad1ef19788
miguelvalle05 <53554524+miguelvalle05@users.noreply.github.com>
2022-11-12 15:47:04 -0600

    db setting

---
+++ Functions.php
@@ -0,0 +1,27 @@
+<?php
+
+function conectar($pdo = false){	
+    
+    if(!$pdo){
+        $mysqli = new mysqli("localhost", "userdb", "dbcoppel", "coppelomnicanal");
+        if ($mysqli->connect_errno) {
+            echo "Fallo al conectar a MySQL1: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        }
+        else {
+            //echo "Fallo al conectar a MySQL2: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+            return $mysqli;
+            }
+    }else{
+        $dbConfig["dbtype"] = "mysql";
+        $dbConfig["dbname"] = "coppelomnicanal";
+        $dbConfig["dbhost"] = "localhost";
+        $dbConfig["dbcharset"] = "utf8";
+        $dbConfig["dbuser"] = "userdb";
+        $dbConfig["dbpass"] = "dbcoppel";
+        $dbh = getPDOConnect($dbConfig);
+        return $dbh;
+    }
+}
+
+
+?>
